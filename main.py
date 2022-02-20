0c379d863cbde96b3184995436ce46b53cee1def
Artur <aat281205@gmail.com>
2022-02-20 14:16:57 +0300

    new commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask import Flask
 from data import db_session
 from config import secret_key, bd_path
 from data.jobs import Jobs
-from data.news import News
 from data.users import User
 
 app = Flask(__name__)
@@ -22,10 +21,46 @@ def main():
     user.speciality = "research engineer"
     user.address = "module_1"
     user.email = "scott_chief@mars.org"
-    user.hashed_password = "cap"
+    user.set_password("cap")
     session.add(user)
     session.commit()
 
+    user = User()
+    user.surname = "Marli"
+    user.name = "Rayner"
+    user.age = 25
+    user.position = "colonist"
+    user.speciality = "dragon pilot"
+    user.address = "module_3"
+    user.email = "Rayner_ray@mars.org"
+    user.set_password("i`m_armored")
+    session.add(user)
+    session.commit()
+
+    user = User()
+    user.surname = "Holmes"
+    user.name = "Sherlock"
+    user.age = 25
+    user.position = "colonist"
+    user.speciality = "detective consultant"
+    user.address = "module_2"
+    user.email = "Sheri_shegi@mars.org"
+    user.set_password('I`m_sherlocked')
+    session.add(user)
+    session.commit()
+
+    session = db_session.create_session()
+    user = User()
+    user.surname = "Watson"
+    user.name = "John"
+    user.age = 28
+    user.position = "colonist"
+    user.speciality = "doctor"
+    user.address = "module_4"
+    user.email = "John_John@mars.org"
+    user.set_password('i_Love_Mary_Morstan')
+    session.add(user)
+    session.commit()
 
     # app.run()
 
